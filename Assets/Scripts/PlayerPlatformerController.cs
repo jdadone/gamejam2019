6f82ed7b0f0e899af2dbbfeee883e38eb3569ea2
Jonathan Dadone <dadone.j@gmail.com>
2019-01-26 09:49:04 -0300

    chore: initial commit

---
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    public float climbingSpeed = 0.01f;
+
+    private bool isNearWall;
+    private bool isClimbing;
+
+    private Vector2 defaultGravity;
+
+    // private SpriteRenderer spriteRenderer;
+    // private Animator animator;
+
+    // Use this for initialization
+    void Awake () 
+    {
+        defaultGravity = Physics2D.gravity;
+        // spriteRenderer = GetComponent<SpriteRenderer> (); 
+        // animator = GetComponent<Animator> ();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis ("Horizontal");
+
+        if (isNearWall && !isClimbing)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                isClimbing = true;
+                Physics2D.gravity = Vector2.zero;
+                velocity.y = 0;
+            }
+        } else if (isClimbing)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                isClimbing = false;
+                Physics2D.gravity = defaultGravity;
+                targetVelocity.x = -5f * maxSpeed;
+                velocity.y = 5f;
+            }
+        } else
+        {
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                velocity.y = jumpTakeOffSpeed;
+            }
+            else if (Input.GetButtonUp("Jump"))
+            {
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * 0.5f;
+                }
+            }
+        }
+
+        /*
+        if(move.x > 0.01f)
+        {
+            if(spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+        } 
+        else if (move.x < -0.01f)
+        {
+            if(spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+
+        animator.SetBool ("grounded", grounded);
+        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+        */
+
+        if (isClimbing)
+        {
+            targetVelocity = Vector2.zero;
+            velocity.y = Input.GetAxis("Vertical");
+        } else
+        {
+            targetVelocity = move * maxSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            isNearWall = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            isNearWall = false;
+
+            isClimbing = false;
+            Physics2D.gravity = defaultGravity;
+        }
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    private Dictionary<ChipType, bool> chips = new Dictionary<ChipType, bool>()
+    {
+        { ChipType.JUMP, false },
+        { ChipType.CLIMB, false },
+        { ChipType.CRAWL, false },
+        { ChipType.FIRE, false }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool HasChip(ChipType chip)
+    {
+        return chips[key: chip];
+    }
+
+    void AddChip (ChipType chip)
+    {
+        chips[chip] = true;
+    }
+}
+
+public enum ChipType { JUMP, CLIMB, CRAWL, FIRE  }
+
+public class Chips
+{
+    public bool jump;
+
+}
