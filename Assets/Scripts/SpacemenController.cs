bab65108d00d92cde77bc1a88619b3bbf5521cc9
Jonathan Dadone <dadone.j@gmail.com>
2019-01-27 14:33:58 -0300

    fix: time errror

--- Assets/Scripts/SpacemenController.cs
+++ Assets/Scripts/SpacemenController.cs
@@ -15,7 +15,7 @@ public class SpacemenController : MonoBehaviour
 	private int numberOfDeaths;
     public string NumberOfNotDeaths { get { return (5 - numberOfDeaths).ToString() + "/5"; } }
 
-    void Start()
+    public void Show ()
     {
         foreach (var astronaut in astronauts)
         {
@@ -27,71 +27,66 @@ public class SpacemenController : MonoBehaviour
 
         Random.InitState(System.DateTime.Now.Second);
 
-        int dead1 = Random.Range(0,5);
-        int dead2 = Random.Range(0,5);
-        int dead3 = Random.Range(0,5);
-        int dead4 = Random.Range(0,5);
+        int dead1 = Random.Range(0, 5);
+        int dead2 = Random.Range(0, 5);
+        int dead3 = Random.Range(0, 5);
+        int dead4 = Random.Range(0, 5);
 
-        while(dead2 == dead1)
+        while (dead2 == dead1)
         {
-			dead2 = Random.Range(0,5);
+            dead2 = Random.Range(0, 5);
         }
 
-        while(dead3 == dead1 || dead3 == dead2)
+        while (dead3 == dead1 || dead3 == dead2)
         {
-        	dead3 = Random.Range(0,5);
+            dead3 = Random.Range(0, 5);
         }
 
-        while(dead4 == dead1 || dead4 == dead2 || dead4 == dead3)
+        while (dead4 == dead1 || dead4 == dead2 || dead4 == dead3)
         {
-        	dead4 = Random.Range(0,5);
+            dead4 = Random.Range(0, 5);
         }
 
-        if(timeSpend < 300)
+        if (timeSpend < 300)
         {
-        	numberOfDeaths = 0;
+            numberOfDeaths = 0;
         }
         else if (timeSpend < 360)
         {
-        	numberOfDeaths = 1;
-        	astronauts[dead1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead1];
+            numberOfDeaths = 1;
+            astronauts[dead1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead1];
         }
         else if (timeSpend < 420)
         {
-        	numberOfDeaths = 2;
-        	astronauts[dead1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead1];
-        	astronauts[dead2].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead2];
+            numberOfDeaths = 2;
+            astronauts[dead1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead1];
+            astronauts[dead2].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead2];
         }
         else if (timeSpend < 480)
         {
-        	numberOfDeaths = 3;
-        	astronauts[dead1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead1];
-        	astronauts[dead2].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead2];
-        	astronauts[dead3].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead3];
+            numberOfDeaths = 3;
+            astronauts[dead1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead1];
+            astronauts[dead2].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead2];
+            astronauts[dead3].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead3];
         }
         else if (timeSpend < 540)
         {
-        	numberOfDeaths = 4;
-        	astronauts[dead1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead1];
-        	astronauts[dead2].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead2];
-        	astronauts[dead3].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead3];
-        	astronauts[dead4].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead4];
+            numberOfDeaths = 4;
+            astronauts[dead1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead1];
+            astronauts[dead2].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead2];
+            astronauts[dead3].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead3];
+            astronauts[dead4].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[dead4];
         }
         else
         {
-        	numberOfDeaths = 5;
-        	astronauts[0].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[0];
-        	astronauts[1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[1];
-        	astronauts[2].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[2];
-        	astronauts[3].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[3];
-        	astronauts[4].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[4];
+            numberOfDeaths = 5;
+            astronauts[0].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[0];
+            astronauts[1].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[1];
+            astronauts[2].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[2];
+            astronauts[3].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[3];
+            astronauts[4].transform.GetComponent<SpriteRenderer>().sprite = deadSprites[4];
         }
 
-        // Debug.Log("Muertos: " + numberOfDeaths + " Te tardaste: " + timeSpend + " Segundos.");
-    }
-
-    public void Show ()
-    {
         StartCoroutine(ShowAnimation());
     }
 
